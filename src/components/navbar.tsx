b2e0333f8b4d389e9afb15edfb5ca3c8700b63b3
timfemey <isholaobafemi@gmail.com>
2024-01-10 15:11:37 +0100

    Update to Resume Google drive link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,13 +9,14 @@ const Navbar: React.FunctionComponent<IAppProps> = (props) => {
         <div className="logo">
           <Link href="/">Femi&apos;s Portfolio</Link>
         </div>
-        
-          
-          <a href="https://drive.google.com/file/d/1-gmQ2TneTzC3_uiTomABlGxkFKgNIpjh/view?usp=drivesdk" className="cta-btn">
-            Resume
-          </a>
-        </div>
-      
+
+        <a
+          href="https://drive.google.com/file/d/1djrX8Z8wWKofIYvqIn7wXHdIRWje9LBh/view?usp=sharing"
+          className="cta-btn"
+        >
+          Resume
+        </a>
+      </div>
     </>
   );
 };
